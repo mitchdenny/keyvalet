084a5a34479e5445d9563cd4248485a14c51a2f1
Mitch Denny <mitch@mitchdenny.com>
2019-06-08 11:53:46 -0700

    Really basic view model scaffolding.

---
+++ src/Keyvalet/ViewModels/KeyvaletViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Keyvalet.ViewModels
+{
+    public abstract class KeyvaletViewModel
+    {
+        public KeyvaletViewModel(Uri uri)
+        {
+            this.Uri = uri;
+        }
+
+        public Uri Uri { get; }
+    }
+}
---
+++ src/Keyvalet/ViewModels/WelcomeViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Keyvalet.ViewModels
+{
+    public class WelcomeViewModel : KeyvaletViewModel
+    {
+        public WelcomeViewModel(Uri uri) : base(uri)
+        {
+        }
+    }
+}
